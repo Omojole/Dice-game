fa3a4d5a66a310fd5a6f7acd57f00461b4932a12
Belleza <aishat.omojole@gmail.com>
2022-09-17 16:11:40 +0100

    working on js

--- dice.js
+++ dice.js
@@ -1,36 +1,51 @@
-'use strict'
-const btnRoll=document.querySelector('.roll');
+"use strict";
+const btnRoll = document.querySelector(".roll");
+const diceImg = document.querySelector(".dice-img");
+const btnHold = document.querySelector(".hold");
+let playing = true;
+let current = 0;
+let activePlayer = 0;
+let scores = [0, 0];
+const switchPlayer = function () {
+  document.querySelector(`.player-0`).classList.toggle("active");
+  document.querySelector(`.player-1`).classList.toggle("active");
 
-const diceImg=document.querySelector('.dice-img');
-const btnHold=document.querySelector('.hold');
-let current=0;
-let activePlayer=0;
-let scores=[0,0];
+  document.getElementById(`totalScore${activePlayer}`).innerHTML =
+    scores[activePlayer];
+  document.getElementById(`currentScore${activePlayer}`).innerHTML = 0;
+  current = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+};
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    current += dice;
+    document.getElementById(`currentScore${activePlayer}`).innerHTML = current;
 
-btnRoll.addEventListener('click',function(){
-const dice=Math.trunc(Math.random()*6)+1;
-current+=dice;
-document.getElementById(`currentScore${activePlayer}`).innerHTML=current;
+    diceImg.classList.remove("hidden");
+    diceImg.src = `/assets/images/dice-${dice}.png`;
+    if (dice === 1) {
+      switchPlayer();
+    }
+  }
+});
 
-diceImg.classList.remove('hidden');
-diceImg.src=`/assets/images/dice-${dice}.png`
-})
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    scores[activePlayer] += current;
+    document.getElementById(`totalScore${activePlayer}`).innerHTML =
+      scores[activePlayer];
 
-btnHold.addEventListener('click',function(){
-    document.querySelector(`.player-0`).classList.toggle('active');
-    document.querySelector(`.player-1`).classList.toggle('active');
+    console.log(scores[activePlayer]);
+    if (scores[activePlayer] >= 30) {
+      playing = false;
+      document
+        .querySelector(`.player-${activePlayer}`)
+        .classList.remove("active");
 
-    scores[activePlayer]+=current
-    document.getElementById(`totalScore${activePlayer}`).innerHTML=scores[activePlayer];
-    document.getElementById(`currentScore${activePlayer}`).innerHTML=0;
-    current=0
-activePlayer=activePlayer===0? 1:0;
-if(scores[activePlayer]>=30){
-    document.querySelector(`.player-${activePlayer}`).classList.remove('active');
-
-    document.querySelector(`.player-${activePlayer}`).classList.add('winner');
-   
-}
-})
-
-    
+      document.querySelector(`.player-${activePlayer}`).classList.add("winner");
+    } else {
+      switchPlayer();
+    }
+  }
+});
